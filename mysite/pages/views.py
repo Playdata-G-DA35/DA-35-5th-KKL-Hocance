44776ac0be2369c67d5159eb1505e92fafde09ad
김주화 <162519312+Crunchrain@users.noreply.github.com>
2024-07-09 16:28:47 +0900

    Update views.py

--- mysite/pages/views.py
+++ mysite/pages/views.py
@@ -2,7 +2,3 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html')
-def review(request):
-    return render(request, 'review.html')
-# def score(request):
-#     return render(request, 'score.html')
